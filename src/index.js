3c46f304816e6adbdec2ffe6324d31cd5e3032bc
squarepancakes <goh.shuyun@gmail.com>
2021-04-03 23:18:51 +0800

    get all nested files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 const fs = require('fs');
+const path = require('path');
 
 const getAllFiles = (dir) => {
   const directoryExists = fs.existsSync(dir);
   if (!directoryExists) {
     return 'No such directory';
   }
-  const printList = fs.readdirSync(dir);
-  return printList;
+
+  let fileList = [];
+  const filesInDir = fs.readdirSync(dir);
+
+  filesInDir.forEach((file) => {
+    const filePath = path.join(dir, file);
+    const stat = fs.lstatSync(filePath);
+    const isDirectory = stat.isDirectory();
+
+    // recursively add to the array
+    if (isDirectory) {
+      const nestedFiles = getAllFiles(filePath);
+      fileList = [...fileList, ...nestedFiles];
+    } else {
+      fileList = [...fileList, filePath];
+    }
+  });
+
+  return fileList;
 };
 
 module.exports = { getAllFiles };
--- test/index.test.js
+++ test/index.test.js
@@ -3,11 +3,19 @@ const { getAllFiles } = require('../src/index.js');
 describe('test', () => {
   it('should print a list of all files in the directory passed in', () => {
     const response = getAllFiles('./src');
-    expect(response).toEqual(['index.js']);
+    expect(response).toEqual(['src/index.js']);
   });
 
   it('should print an error if directory does not exist', () => {
     const response = getAllFiles('./apple');
     expect(response).toBe('No such directory');
+  });
+
+  it('should print all the files including files nested in sub-directories', () => {
+    const response = getAllFiles('./mockData');
+    expect(response).toEqual([
+      'mockData/mockData/mockData.js',
+      'mockData/mockData.js',
+    ]);
   });
 });
