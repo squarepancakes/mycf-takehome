620f34e793447bf3aa13b723374a2d6842e46cbf
squarepancakes <goh.shuyun@gmail.com>
2021-04-04 23:48:37 +0800

    update tests

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ const { INVALID_PATH } = require('../constants/defaultErrorMsg');
 const dir = '/Users/fruitcake/pancakeStack/take-home-test/govtech';
 
 describe('test', () => {
-  it('should print a list of all files in the directory passed in', () => {
+  it('should return a list of all files in the directory passed in', () => {
     const response = getAllFiles('./src');
     expect(response).toEqual([
       'src/getAllFiles.js',
@@ -14,12 +14,12 @@ describe('test', () => {
     ]);
   });
 
-  it('should print an error if path does not exist', () => {
+  it('should return an error if path does not exist', () => {
     const response = getAllFiles('./apple');
     expect(response).toBe(INVALID_PATH);
   });
 
-  it('should print all the files including files nested in sub-directories', () => {
+  it('should return all the files including files nested in sub-directories', () => {
     const response = getAllFiles('./mockData');
     expect(response).toEqual([
       'mockData/mockData/mockData.js',
@@ -28,7 +28,7 @@ describe('test', () => {
     ]);
   });
 
-  it('should return search for files containing TODO no filter provided', () => {
+  it('should search for files containing TODO when no search filter is provided', () => {
     const filesContainingWord = searchFiles('./mockData');
     expect(filesContainingWord).toEqual([
       `${dir}/mockData/mockData/mockData.js`,
